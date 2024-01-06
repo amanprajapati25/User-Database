c1bf1db985df35de34369c14e426de8a7422f41f
aman.frontend <aman.prajapati@staqu.com>
2024-01-07 02:08:12 +0530

    init commit

--- server.js
+++ server.js
@@ -1,18 +1,1 @@
-const express = require('express');
-const app = express();
-const { errorHandler } = require('./middleware/errorHandler');
-const connectDb = require('./config/dbConnection');
-const dotenv = require('dotenv').config();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-const PORT = process.env.PORT || 5000;
-connectDb();
-app.use('/api/contacts', require('./router/GetContactRouter'));
-app.use('/api/user', require('./router/RegistrationRoutes'));
-
-app.use(errorHandler);
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
+console.log("hello")
